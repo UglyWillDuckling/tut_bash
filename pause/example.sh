4c43d49d559bff00edeb19324c8f2a8550c12537
vladimir <vladimi.sedlar@monsoonconsulting.com>
2022-08-21 23:01:03 +0200

    add pause example

---
+++ pause/example.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Function that will get executed when the user presses Ctrl+C
+function handler(){
+    echo "Processing the Ctrl+C"
+    kill $proc_id 
+    exit
+}
+
+# Assign the handler function to the SIGINT signal
+trap handler SIGINT
+
+script(){ #a mock for your script
+    while :; do
+        echo working
+        sleep 2
+    done
+}
+
+set -m #use job control
+script & #run it in the background in a separate process group
+
+proc_id=$(jobs -l | awk '{print $2}')
+while :; do
+    read -sd ' ' #silently read until a space is read
+    kill -STOP $proc_id #stop the background process group
+    sleep 5 #wait 2 seconds (change it to 20 for your case)
+    kill -CONT $proc_id #resume the background process group
+    # fg #put it in the forground so it's killable with Ctrl+C
+done
+
+

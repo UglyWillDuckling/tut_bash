bc985630207f4e63d7909baf53f0c51bf1be16bb
vladimir <vladimi.sedlar@monsoonconsulting.com>
2023-04-14 15:25:23 +0200

    minor updates

--- IFS/arrays.sh
+++ IFS/arrays.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
-IFS='+'
-
-myArray=("1st item" "2nd item" "3rd item" "4th item")
-printf 'Delimited by a +, Word -> %s\n' ${myArray[@]} # output is array items because first char in $IFS not contain space
-
-myArray=("1st item" "2nd item" "3rd item" "4th item")
-printf 'Delimited by a +, uses the * notation: Word -> %s\n' "${myArray[*]}" # use the full array as one word with first char in $IFS as delimeter
-
-myArray=("1st item" "2nd item" "3rd item" "4th item")
-printf 'Delimited by a +, uses @ notation and double quoted: Word -> %s\n' "${myArray[@]}" # use arrays entries, because array variable is quoted
-
+#
+# IFS='+'
+#
+# myArray=("1st item" "2nd item" "3rd item" "4th item")
+# printf 'Delimited by a +, Word -> %s\n' ${myArray[@]} # output is array items because first char in $IFS not contain space
+#
+# myArray=("1st item" "2nd item" "3rd item" "4th item")
+# printf 'Delimited by a +, uses the * notation: Word -> %s\n' "${myArray[*]}" # use the full array as one word with first char in $IFS as delimeter
+#
+# myArray=("1st item" "2nd item" "3rd item" "4th item")
+# printf 'Delimited by a +, uses @ notation and double quoted: Word -> %s\n' "${myArray[@]}" # use arrays entries, because array variable is quoted
+#
 # set IFS to 'e' char
 IFS='e'
 
--- trap/trap.sh
+++ trap/trap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
- 
+
 # Set a trap for SIGINT and SIGTERM signals
-trap "echo The program is terminated." SIGTERM SIGINT
- 
+trap "echo The program is terminated.; exit;" SIGTERM SIGINT
+
 #Display message to generate SIGTERM
 echo "Press Ctrl+Z stop the process"
- 
+
 #Initialize counter variable, i
 i=1
- 
+
 #declare infinite for loop
 while(true)
 do
